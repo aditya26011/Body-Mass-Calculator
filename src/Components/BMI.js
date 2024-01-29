a35514aa281018ced953bf4d1579c47be536a368
Aditya26011 <adityapawar826@gmail.com>
2024-01-29 11:04:33 +0530

    Update on BMI Value

--- src/Components/BMI.js
+++ src/Components/BMI.js
@@ -1,21 +1,21 @@
 import { useState } from "react";
 
 const BMI = () => {
-  const [heightValue, setHeightValue] = useState('');
-  const [weightValue, setWeightValue] = useState('');
-  const[bmiValue,setBMIValue]=useState('');
+  const [heightValue, setHeightValue] = useState("");
+  const [weightValue, setWeightValue] = useState("");
+  const [bmiValue, setBMIValue] = useState("");
 
   const CalculateBmi = () => {
     const height = heightValue;
     const heightInMeters = height / 100;
     const weight = weightValue;
-   setBMIValue(Math.round((weight / (heightInMeters * heightInMeters)) * 10) / 10);
-
+    setBMIValue(
+      Math.round((weight / (heightInMeters * heightInMeters)) * 10) / 10
+    );
   };
 
   return (
     <div className=" bg-white w-8/12 mx-auto rounded-xl min-h-96 shadow-2xl md:w-[68%]  ">
-
       <h2 className="text-center mb-5 font-bold  text-xl">
         Enter Your Details Below
       </h2>
@@ -52,11 +52,25 @@ const BMI = () => {
         </button>
       </div>
 
-       <div className="mt-5 text-center">
-        
-            <h1 className="text-2xl font-boldfont-bold "> {bmiValue ?`Your BMI is ${bmiValue}` :`Welcome !!`}</h1>
-        </div> 
-
+      <div className="mt-5 text-center">
+        {bmiValue ? (
+          <h1 className="text-2xl font-bold">
+            Your BMI is {bmiValue} -{" "}
+            {bmiValue <= 18.5
+              ? "Underweight"
+              : bmiValue >= 18.5 && bmiValue <= 24.5
+              ? "Normal Weight"
+              : bmiValue>=25 && bmiValue<=29.9
+              ? "Overweight"
+              :  "Obesity"
+              }
+          </h1>
+        ) : (
+          <h1 className="text-2xl font-bold">
+            Welcome! Enter your height and weight to see your BMI!
+          </h1>
+        )}
+      </div>
     </div>
   );
 };
